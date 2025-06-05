9717edeb65da55fe17398e566f68bba8f6aacd86
RobertoMoraDiaz04 <robertosagrajas@gmail.com>
2025-06-05 09:37:03 +0200

    checkbox funcionando

--- angular-frontend/src/app/components/landing/modals/view.modal.ts
+++ angular-frontend/src/app/components/landing/modals/view.modal.ts
@@ -1,21 +1,24 @@
-// src/app/view-modal/view-modal.component.ts
-
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 import { Product } from '../../../services/product.service';
 
 @Component({
   selector: 'app-view-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule], 
   templateUrl: './view.modal.html',
-
 })
 export class ViewModalComponent {
   @Input() product: Product | null = null;
   @Output() close = new EventEmitter<void>();
 
   onClose(): void {
+    this.close.emit();
+  }
+
+  guardarCambios(): void {
+    console.log('Producto actualizado:', this.product);
     this.close.emit();
   }
 }
